2a68964ed958197fd4bd516deaa88b1adf9517d1
Aleksey Monakhov <goshagriboedskii@gmail.com>
2022-12-18 19:34:21 +0200

    upd

---
+++ modules&nameSpaces/project-imports/src/decorators/autobind.ts
@@ -0,0 +1,13 @@
+export function AutoBind(_:any, _2:string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    const adjDescriptor = {
+        enumerable: false,
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor
+}
